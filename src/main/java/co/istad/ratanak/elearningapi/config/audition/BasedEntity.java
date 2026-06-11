54de76f4b4b7c680aba3b84acd182ee405f46daa
Ratanak123456 <ratanaksaren3@gmail.com>
2026-06-11 14:42:59 +0700

    testing auditing

--- src/main/java/co/istad/ratanak/elearningapi/config/audition/BasedEntity.java
+++ src/main/java/co/istad/ratanak/elearningapi/config/audition/BasedEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -32,4 +33,7 @@ public abstract class BasedEntity {
 
     @LastModifiedBy
     protected String lastModifiedBy;
+
+
+
 }
